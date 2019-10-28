233bc8781bc8f8f61f9f1fc192c0e97ae907f371
Gabriel Jesus <gabriel.jesus@b2wdigital.com>
2019-10-28 14:17:24 -0300

    DFR01 FAT - Final

--- __main__.py
+++ __main__.py
@@ -1,26 +1,7 @@
-#!/usr/bin/python3
-
 import sys
-from image_dissect import *
-from output_image_dissect import *
+from test_case.dfr01 import *
 
 fileimage = sys.argv[1]
 
 if __name__ == "__main__":
-	mbrDissector = MBRDissector(fileimage)
-	listPartPri = mbrDissector.get_mbr()
-	print(output_mbr(listPartPri))
-	for partPri in listPartPri:
-		fatDissector = FATDissector(partPri, fileimage)
-		fatBoot = fatDissector.boot
-		fatRoot = fatDissector.root
-		for file in fatRoot["FILES"]:
-			if (b'\xe5' in file["FILE_NAME"]):
-				#First cluster of the data section has cluster ID 2
-				print(file)
-				data = fatDissector.get_data_non_fragmented(file)
-				name = file["FILE_NAME"].replace(b'\xe5', b'\x58').decode("utf-8")
-				ext = file["EXT_FILE"].decode("utf-8")
-				f = open(name+"."+ext, "w+")
-				f.write(data.decode("utf-8"))
-				f.close
+	non_fragmented_file_fat(fileimage)
--- dict_enum.py
+++ dict_enum.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 
 from enum import Enum, IntEnum
 
